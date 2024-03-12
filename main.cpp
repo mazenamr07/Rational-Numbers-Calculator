c2db115afa6c6ee61e57777a715355a7effa4209
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2024-03-13 00:34:21 +0200

    updated number_check function

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@ using namespace std;
 // Function to check if input is a valid number
 bool number_check(const string& number) {
     int mini_check = 0;
-    bool digit_count = false;
+//    bool digit_count = false;
+
+    if (number[0] == '.' or number[0] == '/') {
+        return false;
+    }
+    if (number[number.size() - 1] == '.' or number[number.size() - 1] == '/') {
+        return false;
+    }
+
     for (char digit : number) {
-        if ((digit == '.' or digit == '/') and !digit_count) {
-            return false;
-        }
+//        if ((digit == '.' or digit == '/') and !digit_count) {
+//            return false;
+//        }
         if (!isdigit(digit)) {
             if (digit == '.' or digit == '/') {
                 mini_check += 1;
@@ -18,7 +26,7 @@ bool number_check(const string& number) {
                 return false;
             }
         }
-        digit_count = true;
+//        digit_count = true;
     }
     if (mini_check > 1) {
         return false;
@@ -32,9 +40,9 @@ void menu() {
         string choice;
         cout << "Firstly, choose your way of entering numbers (type 0 to exit)\n"
                 "1- 1/2 + 1/4\n"
-                "2- 1/2\n"
-                "   +\n"
-                "   1/4" << endl << ">>";
+                "2- 1/2 (enter)\n"
+                "   +   (enter)\n"
+                "   1/4 (enter)" << endl << ">>";
         getline(cin, choice);
 
         // Checking if input is valid
@@ -56,6 +64,13 @@ void menu() {
             getline(cin, number_1, ' ');
             getline(cin, operation, ' ');
             getline(cin, number_2);
+
+            while (!number_check(number_1) or !number_check(number_2)) {
+                cout << "Operation isn't valid, enter again:";
+                getline(cin, number_1, ' ');
+                getline(cin, operation, ' ');
+                getline(cin, number_2);
+            }
         }
         else if (choice == "2") {
             getline(cin, number_1);
